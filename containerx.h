8aa2b630c64e76e036d14bd78633fd80009eb54f
reza <rzxsism80@gmail.com>
2023-03-02 16:09:20 +0330

    header file containing required function and maros added

---
+++ containerx.h
@@ -0,0 +1,13 @@
+#ifndef CONTAINERX_H
+
+#define CONTAINERX_H
+
+#include <iostream>
+#include <sched.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+char *reserve_stack_memory();
+int child_process(void *agrs);
+int run_process(const char *processName);
+#endif
